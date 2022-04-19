55bb38b489322c4f6d0744607427a265d1c08cc2
Maciej Lipski <maciej.lipski@capgemini.com>
2022-04-19 16:19:30 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/sample/integration/IntegrationApp.java
@@ -0,0 +1,13 @@
+package com.capgemini.sample.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationApp {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(IntegrationApp.class, args);
+    }
+
+}
